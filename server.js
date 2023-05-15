1bf57369d966a7bcde8587d25305f1eb17277b23
thuanphatt <thphatt@gmail.com>
2023-05-15 09:08:54 +0700

    turn on permission file from server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 require("dotenv").config();
-// const cors = require("cors");
+const cors = require("cors");
 const getNowTime = require("./utils/getNowTime");
 const app = express();
-// app.use(cors());
+app.use(cors());
 const http = require("http").createServer(app);
 
 const mongodb = require("mongodb");
@@ -43,25 +43,24 @@ global.socketIO = socketIO;
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-  // res.setHeader("Access-Control-Allow-Credentials", "true");
-  // // Website you wish to allow to connect
-  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-  // // req methods you wish to allow
+  // req methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
 
-  // // req headers you wish to allow
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "X-reqed-With,content-type,Authorization"
-  // );
+  // req headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-reqed-With,content-type,Authorization"
+  );
 
-  // // Set to true if you need the website to include cookies in the reqs sent
-  // // to the API (e.g. in case you use sessions)
-  // res.setHeader("Access-Control-Allow-Credentials", true);
+  // Set to true if you need the website to include cookies in the reqs sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
   // Pass to next layer of middleware
   next();
